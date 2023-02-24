eebab5ae93b1ecad87230838a9bac81fda35232d
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-02-24 20:16:53 +0300

    1

--- 1laba/main.c
+++ 1laba/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 0;
+    int y = 10;
+    char ch;
+
+    while(1) {
+        system("cls"); // очищаем экран
+
+        // выводим персонажа на экран
+        for(int i = 0; i < x; i++) {
+            printf(" ");
+        }
+        printf("O\n");
+
+        // обработка клавиш управления
+        if(kbhit()) {
+            ch = getch();
+            if(ch == 'a') {
+                x--;
+            }
+            if(ch == 'd') {
+                x++;
+            }
+        }
+
+        // задержка
+        for(int i = 0; i < 100000000; i++);
+
+        // движение персонажа вниз
+        y++;
+        if(y >= 20) {
+            y = 10;
+        }
+    }
+
     return 0;
 }
