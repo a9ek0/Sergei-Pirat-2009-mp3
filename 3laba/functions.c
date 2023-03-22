8a04893b8e0942ce5b39d200acb2d471748d955f
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-03-22 11:44:11 +0300

    1

---
+++ 3laba/functions.c
@@ -0,0 +1,5 @@
+//
+// Created by Alexander on 22.03.2023.
+//
+
+#include "functions.h"
---
+++ 3laba/functions.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexander on 22.03.2023.
+//
+
+#ifndef INC_3LABA_FUNCTIONS_H
+#define INC_3LABA_FUNCTIONS_H
+
+#endif //INC_3LABA_FUNCTIONS_H
