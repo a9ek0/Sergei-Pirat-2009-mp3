383b2758f94bf2369b8de8d9ffd49a2b8ed304f7
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-04-08 09:18:16 +0300

    01

--- 4laba/main.c
+++ 4laba/main.c
@@ -28,18 +28,41 @@ NODE *create_node(DATA **data);
 int main() {
     NODE *root;
     DATA *data;
-    data = create_data(NULL, NULL, NULL);
+    data = create_data("It's alive?", "Pudge", "Ilyas");
     root = create_node(&data);
-
+    printf("%s", root->data->question);
+    free(root);
+    free(data);
     return 0;
 }
 
 DATA *create_data(const char *question, const char *answer_yes, const char *answer_no)
 {
-    DATA *data;
-    strcpy(data->answer_no, answer_no);
-    strcpy(data->answer_yes, answer_yes);
+    DATA *data = malloc(sizeof(DATA));
+    if (!data) {
+        return NULL; // failed to allocate memory, return NULL
+    }
+    data->question = malloc(strlen(question) + 1);
+    if (!data->question) {
+        free(data);
+        return NULL; // failed to allocate memory, return NULL
+    }
+    data->answer_yes = malloc(strlen(answer_yes) + 1);
+    if (!data->answer_yes) {
+        free(data->question);
+        free(data);
+        return NULL; // failed to allocate memory, return NULL
+    }
+    data->answer_no = malloc(strlen(answer_no) + 1);
+    if (!data->answer_no) {
+        free(data->answer_yes);
+        free(data->question);
+        free(data);
+        return NULL; // failed to allocate memory, return NULL
+    }
     strcpy(data->question, question);
+    strcpy(data->answer_yes, answer_yes);
+    strcpy(data->answer_no, answer_no);
     return data;
 }
 
@@ -47,9 +70,13 @@ NODE *create_node(DATA **data)
 {
     if(*data == NULL )
         exit(EXIT_FAILURE);
-    NODE *node;
+    NODE *node = malloc(sizeof(NODE));
+    if (!node) {
+        return NULL;
+    }
     node->data = *data;
     node->left = NULL;
     node->right = NULL;
+    return node;
 }
 
